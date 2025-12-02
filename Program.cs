27382a5322f0ad27d8491a5955e1cf954cdca965
rayennasr <rayennasr679@gmail.com>
2025-12-02 14:49:20 +0100

    v1 modification

--- Program.cs
+++ Program.cs
@@ -7,18 +7,19 @@ namespace Rayen_Nasri
     {
         static void Main(string[] args)
         {
-            int[] T = [];
+            int[] T = new int[51];
             int i = 0;
             Random random = new Random();
             while (i <= 50)
             {
                 // remplir le tableau de manier aliatoire
                 int n = random.Next(0, 10);
-                if (n != -1)
+                if (n == -1)
                 {
                     break;
                 }
                 T[i] = n;
+                i++;
             }
             Console.WriteLine("Un entier ");
             int val = int.Parse(Console.ReadLine());
@@ -36,38 +37,36 @@ namespace Rayen_Nasri
             
 
 
-        }
         static bool Doublon(int[] T, int val, int n)
         {
             int count = 0;
-            bool exist = false;
             for (int i = 0; i < n; i++)
             {
                 if (T[i] == val)
                 {
                     count++;
                 }
-                return count > 1;
             }
-            return exist;
+            return count > 1;
+        }
         }
 
         static int DeuxiemPlusPetit(int[] T, int n)
         {
             int premierPetitentier = T[0];
-            int deuxiemPlusPetit = premierPetitentier;
-            for ( int i = 0; i < n;i++)
+            int deuxiemPlusPetit = int.MaxValue;
+            for (int i = 0; i < n; i++)
             {
-                if (premierPetitentier > T[i + 1])
+                if (premierPetitentier > T[i])
                 {
-                    premierPetitentier = T[i + 1];
+                    premierPetitentier = T[i];
                 }
             }
             for (int i = 0; i < n; i++)
             {
-                if (deuxiemPlusPetit > T[i + 1] && deuxiemPlusPetit!= premierPetitentier)
+                if (deuxiemPlusPetit > T[i] && deuxiemPlusPetit != premierPetitentier && T[i] != premierPetitentier)
                 {
-                    deuxiemPlusPetit = T[i + 1];
+                    deuxiemPlusPetit = T[i];
                 }
             }
 
